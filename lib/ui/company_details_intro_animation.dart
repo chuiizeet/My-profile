a7c02f269588a6e8776c6a30b925053ee4743b58
imac <imac@Dev.local>
2018-11-13 00:55:02 -0600

    Add: More animations

--- lib/ui/company_details_intro_animation.dart
+++ lib/ui/company_details_intro_animation.dart
@@ -21,8 +21,32 @@ class CompanyDetailsIntroAnimation {
   nameOpacity = new Tween(begin: 0.0, end: 1.0)
   .animate(new CurvedAnimation(parent: controller,
   curve: new Interval(0.350, 0.450,
-  curve: Curves.easeIn)));
+  curve: Curves.easeIn))),
 
+  locationOpacity = new Tween(begin: 0.0, end: 0.8)
+  .animate(new CurvedAnimation(parent: controller,
+  curve: new Interval(0.500, 0.600,
+  curve: Curves.easeIn))),
+
+  dividerWidth = new Tween(begin: 0.0, end: 225.0)
+  .animate(new CurvedAnimation(parent: controller,
+  curve: new Interval(0.650, 0.750,
+  curve: Curves.fastOutSlowIn))),
+
+  aboutOpacity = new Tween(begin: 0.0, end: 0.850)
+  .animate(new CurvedAnimation(parent: controller,
+  curve: new Interval(0.750, 0.900,
+  curve: Curves.easeIn))),
+
+  projectScrollerXTranslation = new Tween(begin: 60.0, end: 0.0)
+  .animate(new CurvedAnimation(parent: controller,
+  curve: new Interval(0.830, 1.000,
+  curve: Curves.ease))),
+
+  projecScrollerOpacity = new Tween(begin: 0.0, end: 1.0)
+  .animate(new CurvedAnimation(parent: controller,
+  curve: new Interval(0.830, 1.000,
+  curve: Curves.fastOutSlowIn)));
 
 
 
@@ -32,11 +56,11 @@ class CompanyDetailsIntroAnimation {
   final Animation<double> bgDropBlur;
   final Animation<double> avatarSize;
   final Animation<double> nameOpacity;
-  // final Animation<double> locationOpacity;
-  // final Animation<double> dividerWidth;
-  // final Animation<double> aboutOpacity;
-  // final Animation<double> projectScrollerXTranslation;
-  // final Animation<double> projecScrollerOpacity;
+  final Animation<double> locationOpacity;
+  final Animation<double> dividerWidth;
+  final Animation<double> aboutOpacity;
+  final Animation<double> projectScrollerXTranslation;
+  final Animation<double> projecScrollerOpacity;
 
 
 
